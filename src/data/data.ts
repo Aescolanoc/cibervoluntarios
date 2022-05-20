f9bfd82bc98b2b5a83eb170b1bc8eca5c056d417
Mel Escolano <amalia.escolano@gmail.com>
2022-05-20 12:23:38 +0200

    Created data and reorganized img folder

---
+++ src/data/data.ts
@@ -0,0 +1,87 @@
+export const ods = [
+  {
+    id: "1",
+    img: "S-WEB-Goal-01.png",
+    selected: false,
+  },
+  {
+    id: "2",
+    img: "S-WEB-Goal-02.png",
+    selected: false,
+  },
+  {
+    id: "3",
+    img: "S-WEB-Goal-03.png",
+    selected: false,
+  },
+  {
+    id: "4",
+    img: "S-WEB-Goal-04.png",
+    selected: false,
+  },
+  {
+    id: "5",
+    img: "S-WEB-Goal-05.png",
+    selected: false,
+  },
+  {
+    id: "6",
+    img: "S-WEB-Goal-06.png",
+    selected: false,
+  },
+  {
+    id: "7",
+    img: "S-WEB-Goal-07.png",
+    selected: false,
+  },
+  {
+    id: "8",
+    img: "S-WEB-Goal-08.png",
+    selected: false,
+  },
+  {
+    id: "9",
+    img: "S-WEB-Goal-09.png",
+    selected: false,
+  },
+  {
+    id: "10",
+    img: "S-WEB-Goal-10.png",
+    selected: false,
+  },
+  {
+    id: "11",
+    img: "S-WEB-Goal-11.png",
+    selected: false,
+  },
+  {
+    id: "12",
+    img: "S-WEB-Goal-12.png",
+    selected: false,
+  },
+  {
+    id: "13",
+    img: "S-WEB-Goal-13.png",
+    selected: false,
+  },
+  {
+    id: "14",
+    img: "S-WEB-Goal-14.png",
+    selected: false,
+  },
+  {
+    id: "15",
+    img: "S-WEB-Goal-15.png",
+    selected: false,
+  },
+  {
+    id: "16",
+    img: "S-WEB-Goal-16.png",
+    selected: false,
+  },
+  {
+    id: "17",
+    img: "S-WEB-Goal-17.png",
+    selected: false,
+  },
+];
