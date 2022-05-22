4adbeda4399a09f15e30916a67a7015786f359e4
Mel Escolano <amalia.escolano@gmail.com>
2022-05-22 15:04:58 +0200

    Update userStep component

--- src/components/UserStep.vue
+++ src/components/UserStep.vue
@@ -3,12 +3,14 @@
     <h5>Paso 1 de 4 - Datos personales</h5>
     <div class="form-floating mb-3">
       <input type="text" class="form-control" id="name" v-model="store.userData.name" maxlength="15" required />
-      <label for="name">Nombre</label>
+      <label for="name">Nombre*</label>
     </div>
     <div class="form-floating mb-3">
       <input type="text" class="form-control" id="lastName" v-model="store.userData.lastName" maxlength="20" required />
-      <label for="lastName">Apellidos</label>
+      <label for="lastName">Apellidos*</label>
+      <p class="data-info">* Campos obligatorios</p>
     </div>
+
     <div class="mb-3">
       <label for="comment" class="form-label">Observaciones:</label>
       <textarea class="form-control" id="comment" v-model="store.userData.comment" rows="4" maxlength="280"></textarea>
@@ -30,9 +32,11 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-.textarea-info {
+.textarea-info,
+.data-info {
   color: rgb(168, 167, 167);
   font-size: 12px;
+  text-align: end;
 }
 
 @media (min-width: 700px) {
